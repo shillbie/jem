9a5e5cad0d0a80a46c9b1a571988d81c12d47c97
sakura <okaoka1567@gmail.com>
2020-12-13 00:15:17 +0900

    add: line client

--- client.go
+++ client.go
@@ -1,1 +1,24 @@
 package linego
+
+import (
+	"context"
+	api "github.com/sakura-rip/linego/lineapi"
+)
+
+type LineClient struct {
+	Talk *api.TalkServiceClient
+	Poll *api.TalkServiceClient
+
+	ctx     context.Context
+	appType api.AppType
+	Profile *api.Profile
+}
+
+func NewLineClient(appType api.AppType) *LineClient {
+	return &LineClient{
+		Talk:    nil,
+		Poll:    nil,
+		appType: appType,
+		ctx:     context.Background(),
+	}
+}
