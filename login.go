0196e029e894a420574cb5bc10b57952796811f2
sakura <okaoka1567@gmail.com>
2020-12-13 16:45:17 +0900

    add: sec qr login func

--- login.go
+++ login.go
@@ -20,7 +20,26 @@ func NewQrLoginClient() *QrLoginClient {
 	}
 }
 func (cl *LineClient) loginViaQrCode() {
+	qrL := NewQrLoginClient()
+	qrL.createLoginSession1()
+	qrL.CreateQrSession()
+	qrL.createLoginSession2()
+	url, er := qrL.CreateQrCode()
+	if er != nil {
+		log.Printf("%+v\n", er)
+	}
+	fmt.Println("login this url on your mobile : " + url)
 
+	qrL.WaitForQrCodeVerified()
+	err := qrL.CertificateLogin("")
+	if err != nil {
+		pin, _ := qrL.CreatePinCode()
+		fmt.Println("input this pin code on your mobile : " + pin)
+		qrL.WaitForInputPinCode()
+	}
+	token, _, _ := qrL.QrLogin()
+
+	cl.LoginViaToken(token)
 }
 
 func (cl *LineClient) loginViaMail(mail, passwd string) {
