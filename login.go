19011022b9d4fab002561b02a31f77712d3a1839
sakura <okaoka1567@gmail.com>
2020-12-13 16:44:46 +0900

    add: login via token func

--- login.go
+++ login.go
@@ -1,6 +1,11 @@
 package linego
 
 func (cl *LineClient) LoginViaToken(authToken string) {
+	cl.Talk = createTalkService(LineHost+TalkPath, cl.getDefaultHeader(authToken))
+	cl.Poll = createTalkService(LineHost+PollPath, cl.getDefaultHeader(authToken))
+
+	cl.BeforeLogin()
+}
 
 }
 func (cl *LineClient) loginViaQrCode() {
