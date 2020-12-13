c6723f6b10798ab40d8b64c66cff76526cf05494
sakura <okaoka1567@gmail.com>
2020-12-13 18:08:00 +0900

    fix: before login

--- login.go
+++ login.go
@@ -12,8 +12,6 @@ import (
 func (cl *LineClient) LoginViaToken(authToken string) {
 	cl.Talk = createTalkService(LineHost+TalkPath, cl.getDefaultHeader(authToken))
 	cl.Poll = createTalkService(LineHost+PollPath, cl.getDefaultHeader(authToken))
-
-	cl.BeforeLogin()
 }
 
 type QrLoginClient struct {
