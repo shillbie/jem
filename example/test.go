bf2b89fd1bcac834927edfb2b70519a4c90b2446
sakura <okaoka1567@gmail.com>
2020-12-14 14:34:04 +0900

    add: handle operation example

--- example/test.go
+++ example/test.go
@@ -4,13 +4,16 @@ import (
 	"github.com/sakura-rip/linego"
 	"github.com/sakura-rip/linego/talkservice"
 	"log"
+	"strings"
 )
 
 var bot *linego.LineClient
 
 func main() {
 	bot = linego.NewLineClient(talkservice.AppType_ANDROIDLITE)
-	bot.Login()
+	bot.Login() //QRLogin
+	//bot.Login("mail", "passwd") //email
+	//bot.Login("Token") //token login
 	bot.SendMessage("ud5b1c4b637d684f5714fc4dd2e585565", "login success")
 	for {
 		ops, err := bot.FetchOps()
@@ -21,11 +24,29 @@ func main() {
 					bot.SetIndividualRev(op)
 				} else {
 					bot.SetRevision(op.Revision)
-					log.Printf("%+v\n", op)
+					HandleOperation(op)
 				}
 			}
 		} else {
 			log.Printf("%+v\n", err)
+		}
+	}
+}
+
+func HandleOperation(op *talkservice.Operation) {
+	log.Printf("%+v\n", op)
+	switch op.Type {
+	case talkservice.OperationType_RECEIVE_MESSAGE, talkservice.OperationType_SEND_MESSAGE:
+		msg := op.Message
+		if msg.ContentType == talkservice.ContentType_NONE {
+			if msg.Text == "test" {
+				bot.SendMessage(msg.To, "Yes")
+			}
+		}
+	case talkservice.OperationType_NOTIFIED_INVITE_INTO_CHAT:
+		if strings.Contains(op.Param3, bot.Profile.Mid) {
+			bot.AcceptChatInvitation(op.Param1)
+			bot.SendMessage(op.Param1, "Hi there")
 		}
 	}
 }
