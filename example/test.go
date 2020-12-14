4ad1aab7f20b56bb63bdc737b7c6ab70c2b8df84
sakura <okaoka1567@gmail.com>
2020-12-14 14:18:42 +0900

    add: fetch operation func

--- example/test.go
+++ example/test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/sakura-rip/linego"
 	"github.com/sakura-rip/linego/talkservice"
+	"log"
 )
 
 var bot *linego.LineClient
@@ -11,4 +12,20 @@ func main() {
 	bot = linego.NewLineClient(talkservice.AppType_ANDROIDLITE)
 	bot.Login()
 	bot.SendMessage("ud5b1c4b637d684f5714fc4dd2e585565", "login success")
+	for {
+		ops, err := bot.FetchOps()
+		if err == nil {
+			for _, op := range ops {
+				if op.Type == talkservice.OperationType_END_OF_OPERATION {
+					bot.SetGlobalRev(op)
+					bot.SetIndividualRev(op)
+				} else {
+					bot.SetRevision(op.Revision)
+					log.Printf("%+v\n", op)
+				}
+			}
+		} else {
+			log.Printf("%+v\n", err)
+		}
+	}
 }
