bd97c6f4ab041e582307eb82fed9386a1440a10e
sakura <okaoka1567@gmail.com>
2020-12-14 17:18:46 +0900

    add: some example

--- example/test.go
+++ example/test.go
@@ -10,11 +10,16 @@ import (
 var bot *linego.LineClient
 
 func main() {
-	bot = linego.NewLineClient(talkservice.AppType_ANDROIDLITE)
+	bot = linego.NewLineClient(talkservice.AppType_IOS)
 	bot.Login() //QRLogin
 	//bot.Login("mail", "passwd") //email
 	//bot.Login("Token") //token login
-	bot.SendMessage("ud5b1c4b637d684f5714fc4dd2e585565", "login success")
+	bot.DumpBotData()
+	_, err := bot.SendMessage("ud5b1c4b637d684f5714fc4dd2e585565", "login success")
+	if err != nil {
+		log.Printf("%+v\n", err)
+	}
+
 	for {
 		ops, err := bot.FetchOps()
 		if err == nil {
@@ -40,13 +45,23 @@ func HandleOperation(op *talkservice.Operation) {
 		msg := op.Message
 		if msg.ContentType == talkservice.ContentType_NONE {
 			if msg.Text == "test" {
-				bot.SendMessage(msg.To, "Yes")
+				_, err := bot.SendMessage(msg.To, "Yes")
+				if err != nil {
+					log.Printf("%+v\n", err)
+				}
 			}
 		}
-	case talkservice.OperationType_NOTIFIED_INVITE_INTO_CHAT:
+	case talkservice.OperationType_NOTIFIED_INVITE_INTO_CHAT, talkservice.OperationType_NOTIFIED_INVITE_INTO_GROUP:
 		if strings.Contains(op.Param3, bot.Profile.Mid) {
-			bot.AcceptChatInvitation(op.Param1)
-			bot.SendMessage(op.Param1, "Hi there")
+			err := bot.AcceptChatInvitation(op.Param1)
+			if err != nil {
+				log.Printf("%+v\n", err)
+			}
+			_, err = bot.SendMessage(op.Param1, "Hi there")
+			if err != nil {
+				log.Printf("%+v\n", err)
+			}
 		}
 	}
+	bot.DumpBotData()
 }
